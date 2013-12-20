13235aabe6e49dfe73b63c35fc35ce08fa95512f
Matt Jaeger <matthew.jaeger@gmail.com>
2013-12-19 21:23:49 -0800

    Run subscribe / unsubscribe off the main thread.

--- src/android/ParsePushReceiver.java
+++ src/android/ParsePushReceiver.java
@@ -18,24 +18,34 @@ public class ParsePushReceiver extends CordovaPlugin {
 		boolean result = false;
 		
 		if(action.equals(ACTION_SUBSCRIBE)) {
-			result = subscribe(args.getString(0));
+			result = subscribe(args.getString(0), callbackContext);
 		}
 		else if(action.equals(ACTION_UNSUBSCRIBE)) {
-			result = unsubscribe(args.getString(0));
+			result = unsubscribe(args.getString(0), callbackContext);
 		}
 				
 		return result;
 	}
 	
-	public boolean subscribe(String channel)
+	public boolean subscribe(String channel, CallbackContext callbackContext)
 	{
-		PushService.subscribe(this.cordova.getActivity(), channel, YOUR_ACTIVITY_CLASS_NAME.class); //TODO: dynamically choose class		
+		cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+            	PushService.subscribe(this.cordova.getActivity(), channel, YOUR_ACTIVITY_CLASS_NAME.class); //TODO: dynamically choose class	
+                callbackContext.success(); // Thread-safe.
+            }
+        });			
 		return true;
 	}
 	
-	public boolean unsubscribe(String channel)
+	public boolean unsubscribe(String channel, CallbackContext callbackContext)
 	{
-		PushService.unsubscribe(this.cordova.getActivity(), channel);
+		cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+            	PushService.unsubscribe(this.cordova.getActivity(), channel);	
+                callbackContext.success(); // Thread-safe.
+            }
+        });			
 		return true;
 	}
 }
